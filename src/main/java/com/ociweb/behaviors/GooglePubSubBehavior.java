74d1a03ff4e765f99f64c5d1b3302f2b5e5c84fb
David Giovannini <dave@David.local>
2017-07-31 15:04:38 -0500

    wire up google channel

--- src/main/java/com/ociweb/behaviors/GooglePubSubBehavior.java
+++ src/main/java/com/ociweb/behaviors/GooglePubSubBehavior.java
@@ -1,15 +1,18 @@
 package com.ociweb.behaviors;
 
+import com.ociweb.gl.api.HTTPResponseListener;
+import com.ociweb.gl.api.HTTPResponseReader;
 import com.ociweb.gl.api.PubSubListener;
 import com.ociweb.iot.maker.FogCommandChannel;
 import com.ociweb.iot.maker.FogRuntime;
 import com.ociweb.pronghorn.pipe.BlobReader;
 
-public class GooglePubSubBehavior implements PubSubListener {
+public class GooglePubSubBehavior implements PubSubListener, HTTPResponseListener {
     private FogCommandChannel cmd;
 
     public GooglePubSubBehavior(FogRuntime runtime) {
-        this.cmd = runtime.newCommandChannel(/*NET_REQUESTER*/);
+        this.cmd = runtime.newCommandChannel(NET_REQUESTER);
+        this.cmd.ensureHTTPClientRequesting(10, 2048);
     }
 
     @Override
@@ -17,6 +20,11 @@ public class GooglePubSubBehavior implements PubSubListener {
         String body = messageReader.readUTF();
         System.out.println(String.format("F) %s", body));
         // TODO: send to google - cmd.httpPost("google", 42, )
+        return true;
+    }
+
+    @Override
+    public boolean responseHTTP(HTTPResponseReader reader) {
         return true;
     }
 }
