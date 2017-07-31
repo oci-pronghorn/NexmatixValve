e7ea1f18a18e8517c8254ef5d7664e3ae76fc000
David Giovannini <dave@David.local>
2017-07-31 16:13:05 -0500

    trying to push past the 401

--- src/main/java/com/ociweb/behaviors/GooglePubSubBehavior.java
+++ src/main/java/com/ociweb/behaviors/GooglePubSubBehavior.java
@@ -8,7 +8,12 @@ import com.ociweb.iot.maker.FogRuntime;
 import com.ociweb.pronghorn.pipe.BlobReader;
 import com.ociweb.pronghorn.util.Appendables;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class GooglePubSubBehavior implements PubSubListener, HTTPResponseListener {
     private final FogCommandChannel cmd;
@@ -19,7 +24,7 @@ public class GooglePubSubBehavior implements PubSubListener, HTTPResponseListene
         this.cmd.ensureHTTPClientRequesting(10, 2048);
 
         // https://pubsub.googleapis.com/v1/projects/myproject/topics/mytopic:publish
-        this.url = String.format("https://pubsub.googleapis.com/v1/projects/%s/topics/%s:publish", "nexamatixmvp", "manifold-state");
+        this.url = String.format("https://pubsub.googleapis.com/v1/projects/%s/topics/%s:publish", "nexmatixmvp", "manifold-state");
     }
 
     @Override
@@ -50,11 +55,55 @@ public class GooglePubSubBehavior implements PubSubListener, HTTPResponseListene
         System.out.println(String.format("F) %s", builder.toString()));
 
         //cmd.httpPost(url, 80);
+        try {
+            theOldWay(url, builder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
     @Override
     public boolean responseHTTP(HTTPResponseReader reader) {
         return true;
+    }
+
+    private void theOldWay(String url, String body) throws IOException {
+       // final String USER_AGENT = "Mozilla/5.0";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        //con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        //String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        //wr.writeBytes(urlParameters);
+        wr.writeBytes(body);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        //System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
     }
 }
