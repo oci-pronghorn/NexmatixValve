d51b226016edfd3661298cc2fde18371d78aa9fb
Phillip LaBanca <labancap@ociweb.com>
2017-10-10 15:56:59 -0500

    cleanup

--- src/main/java/com/ociweb/NexmatixValve.java
+++ src/main/java/com/ociweb/NexmatixValve.java
@@ -7,19 +7,13 @@ import com.ociweb.behaviors.simulators.SerialSimulatorBehavior;
 import com.ociweb.behaviors.*;
 import com.ociweb.gl.api.MQTTBridge;
 import com.ociweb.iot.maker.*;
-import jnr.ffi.provider.LoadedLibrary;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.List;
-import java.util.Map;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -140,7 +134,4 @@ public class NexmatixValve implements FogApp
         //runtime.bridgeTransmission("DDS", this.ddsBridge);
     }
 
-
-    private static class JarNameFactory {
-    }
 }
