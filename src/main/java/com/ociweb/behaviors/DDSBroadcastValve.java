ba2ca1642270ce0cd3efa57029e8c5978800dd06
Phillip LaBanca <labancap@ociweb.com>
2017-10-17 17:26:07 -0500

    Refactored DDSBroadcastValve:message() to init valveData in order to validate valveDataDataWriter.register_instance() and valveDataDataWriter.write operations

--- src/main/java/com/ociweb/behaviors/DDSBroadcastValve.java
+++ src/main/java/com/ociweb/behaviors/DDSBroadcastValve.java
@@ -88,6 +88,8 @@ public class DDSBroadcastValve implements PubSubListener, StartupListener, Shutd
             return;
         }
 
+        setPublisherQOS(publisher);
+
         //if (true == setPublisherQOS(publisher)){ }
         dataWriter = publisher.create_datawriter(
             topic,
@@ -162,34 +164,96 @@ public class DDSBroadcastValve implements PubSubListener, StartupListener, Shutd
         }
         return false;
     }
+    private void initValveData(ValveData valveData){
+//            public int manifoldId;                   // nil
+//            public int stationId;                    // "st"
+//            public int valveSerialId;                // nil
+//            public String partNumber;                // nil
+//            public boolean leakFault;                // nil
+//            public PresureFault pressureFault;       // "pf"
+//            public boolean valveFault;               // nil
+//            public int cycles;                       // "cc"
+//            public int pressure;                     // nil
+//            public int durationLast12;               // nil
+//            public int durationLast14;               // nil
+//            public int equalizationAveragePressure;  // nil
+//            public int equalizationPressureRate;     // nil
+//            public int residualOfDynamicAnalysis;    // nil
+//            public int suppliedPressure;             // nil
+
+        valveData.manifoldId = 1;
+        valveData.stationId  = 2;
+        valveData.valveSerialId = 3;
+        valveData.partNumber = "1234";
+        valveData.leakFault = false;
+        valveData.pressureFault = PresureFault.NO_FAULT;
+        valveData.cycles = 4;
+        valveData.pressure = 5;
+        valveData.durationLast12 =6;
+        valveData.durationLast14 =7;
+        valveData.equalizationAveragePressure =8;
+        valveData.residualOfDynamicAnalysis =9;
+        valveData.suppliedPressure =10;
+    }
+    private void printValveData(ValveData valveData) {
+        System.out.println("manifoldId:" + valveData.manifoldId);
+        System.out.println("stationId:" + valveData.stationId);
+        System.out.println("valveSerialId:" + valveData.valveSerialId);
+        System.out.println("partNumber:" + valveData.partNumber);
+        System.out.println("leakFault:" + valveData.leakFault);
+        System.out.println("pressureFault:" + valveData.pressureFault.toString());
+        System.out.println("cycles:" + valveData.cycles);
+        System.out.println("pressure:" + valveData.pressure);
+        System.out.println("durationLast12:" + valveData.durationLast12);
+        System.out.println("durationLast14:" + valveData.durationLast14);
+        System.out.println("equalizationAveragePressure:" + valveData.equalizationAveragePressure);
+        System.out.println("residualOfDynamicAnalysis:" + valveData.residualOfDynamicAnalysis);
+        System.out.println("suppliedPressure:" + valveData.suppliedPressure);
+    }
 
     public boolean message(CharSequence charSequence, ChannelReader channelReader) {
+        boolean rc = false;
         if (this.dataWriter != null) {
             ValveData valveData = (ValveData)channelReader.readObject();
+            initValveData(valveData);
+            //printValveData(valveData);
             ValveDataDataWriter valveDataDataWriter = ValveDataDataWriterHelper.narrow(this.dataWriter);
+
             if (valveDataDataWriter != null) {
-                // TODO: fix this
-                //int handle = valveDataDataWriter.register_instance(valveData);
-                //int rc = valveDataDataWriter.write(valveData, 0);
-                int rc = RETCODE_OK.value;
-                if (RETCODE_OK.value == rc) {
-                    return true;
+
+                int handle = valveDataDataWriter.register_instance(valveData);
+                System.out.println("register_instance:" + handle);
+
+                int write_rc = valveDataDataWriter.write(valveData, handle);
+                switch(write_rc){
+                    case RETCODE_OK.value:
+                        System.out.println("valveDataDataWriter.write returned:" + "RETCODE_OK");
+                        rc = true;
+                        break;
+                    case RETCODE_TIMEOUT.value:
+                        System.out.println("valveDataDataWriter.write returned:" + "RETCODE_TIMEOUT");
+                        break;
+                    default:
+                        System.out.println("valveDataDataWriter.write returned:" + write_rc);
+                        break;
                 }
             }
         }
-        return false;
+        return rc;
     }
 
     @Override
     public boolean acceptShutdown() {
+        boolean rc = false;
         System.out.println("Publisher exiting");
         // Clean up
         if (domainParticipant != null) {
             domainParticipant.delete_contained_entities();
             domainParticipantFactory.delete_participant(domainParticipant);
+            rc = true;
         }
         TheServiceParticipant.shutdown();
-        return true;
+        return rc;
     }
 }
 
